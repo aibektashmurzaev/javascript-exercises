e1ce3d51e81fbd59d941a618a623525621270cbc
Aibek <aibektashmurzaev1929@gmail.com>
2024-10-20 11:39:53 +0200

    updated the '02_repeatString' by completing the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+  let updatedWord = '';
+  if(number > 0){
+    for(let i=0; i<number; i++){
+        updatedWord += word;
+     }
+  }else if(number == 0){
+     return '';
+  }else{
+    return 'ERROR';
+  }
+  return updatedWord;
 };
 
 // Do not edit below this line
