afdb3a0e1d465fba2e22ea2d7b94c35ac4b02351
Aibek <aibektashmurzaev1929@gmail.com>
2024-10-20 14:33:10 +0200

    Completed the '03_reverseString' exercise'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let arr = [];
+    for(let i=0; i<text.length; i++){
+        arr.push(text[i]);
+    }
+    arr.reverse();
+    return arr.join('');
 };
+
+//I need to take the last character
 
 // Do not edit below this line
 module.exports = reverseString;
