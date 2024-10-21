1f02bb19517849284489c7c3030e5feb2c6e5a88
Aibek <aibektashmurzaev1929@gmail.com>
2024-10-21 08:57:49 +0200

    Updated the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // const newArr = [];
+
+    // arr.forEach((item) => {
+    //     if(!args.includes(item)){
+    //         newArr.push(item);
+    //     }
+    // });
+
+    // return newArr;
+    return arr.filter(val => !args.includes(val))
+
 };
 
 // Do not edit below this line
