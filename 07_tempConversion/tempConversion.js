7a9128cf2418c11f31e67835df3ced63d13c61b2
Aibek <aibektashmurzaev1929@gmail.com>
2024-10-22 18:49:29 +0200

    Updated the tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahreneint) {
+  const celsius = (fahreneint - 32)*5/9;
+  
+  return Number.isInteger(celsius) ? celsius : parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahr = celsius*(9/5)+32;
+
+  return Number.isInteger(fahr) ? fahr : parseFloat(fahr.toFixed(1));
 };
 
 // Do not edit below this line
