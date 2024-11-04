8040c758a0da8459e7415df5a4d1caaa6780e103
Aibek <aibektashmurzaev1929@gmail.com>
2024-11-04 18:23:36 +0100

    Solution for a 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphabetNumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const filteredString = string.toLowerCase().split('').filter((character) => alphabetNumeric.includes(character)).join('');
+
+    const reversedString = filteredString.split('').reverse().join('');
+
+    return filteredString === reversedString;
 };
 
 // Do not edit below this line
